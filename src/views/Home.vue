aaeeddbeea794afb5af713ad0be28bb3afdbaae0
mostafa hassan <mostafahassan999888@gmail.com>
2021-02-21 21:52:25 +0200

    Added Navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="home">
+    <navbar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Navbar from '../components/Navbar.vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    Navbar,
   },
 };
 </script>
