e93e0cfa1ac59c59c6f6126c19610bfafd9b5097
mostafa hassan <mostafahassan999888@gmail.com>
2021-02-21 22:42:29 +0200

    Added movieList

---
+++ src/components/MovieList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="movie-container">
+       Movie list
+      <ul>
+          <li v-for="(movie, index) in movies" :key="movie.id">
+            index: {{index}}  {{movie.name}}
+          </li>
+      </ul>
+      
+      </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            movies : [],
+        }
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+	#movie-container {
+		padding-top: 1rem;
+		display: flex;
+		flex-wrap: wrap;
+		flex-grow: 2;
+		justify-content: center;
+	}
+</style>
