819f3a690fdcc0945e7c188015324fda72c6d374
mostafa hassan <mostafahassan999888@gmail.com>
2021-02-21 22:33:50 +0200

    Added sidebar and filters

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,38 @@
 <template>
   <div class="home">
     <navbar />
+    <div id="main-section">
+      <sidebar />
+
+      <movie-list />
+    </div>
   </div>
 </template>
 
 <script>
+import MovieList from "../components/MovieList.vue";
 // @ is an alias to /src
-import Navbar from '../components/Navbar.vue';
+import Navbar from "../components/Navbar.vue";
+import Sidebar from "../components/Sidebar.vue";
 
 export default {
   name: "Home",
   components: {
     Navbar,
+    Sidebar,
+    MovieList,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+	.home {
+		display: flex;
+		flex-direction: column;
+
+		#main-section {
+			display: flex;
+			margin-top: 50px;
+		}
+	}
+</style>
