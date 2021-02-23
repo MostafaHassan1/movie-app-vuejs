39c5e469432e21be81e4f82922b6cea3ae151cfc
mostafa hassan <mostafahassan999888@gmail.com>
2021-02-23 19:54:52 +0200

    Added filtering functionality

--- src/store/Modules/MoviesModule.js
+++ src/store/Modules/MoviesModule.js
@@ -1,31 +1,58 @@
 import movieList from "../../assets/movie-list";
-const SET_SEARCH = "SET_SEARCH"
+const SET_SEARCH = "SET_SEARCH";
+const SET_FILTER = "SET_FILTER";
 const state = {
-    movies: movieList,
-    search : ''
-}
+  movies: movieList,
+  search: "",
+  filter: {
+    key: "rating",
+    order: "desc",
+  },
+};
 
 const mutations = {
-    [SET_SEARCH](state, search){
-        state.search = search
-    }
-}
+  [SET_SEARCH](state, search) {
+    state.search = search;
+  },
+  [SET_FILTER](state, filter) {
+    state.filter = filter;
+  },
+};
 
 const actions = {
-    search({commit}, search){
-        commit(SET_SEARCH, search)
-    }
-}
+  search({ commit }, search) {
+    commit(SET_SEARCH, search);
+  },
+  filter({ commit }, filter) {
+    commit(SET_FILTER, filter);
+  },
+};
 
 const getters = {
-    getMovies: state => {
-        return state.movies.filter(movie => movie.name.toLowerCase().indexOf(state.search.toLowerCase()) > -1)
-    }
-}
+  getMovies: (state) => {
+    return state.movies
+      .filter(
+        (movie) =>
+          movie.name.toLowerCase().indexOf(state.search.toLowerCase()) > -1
+      )
+      .sort(compare(state.filter));
+  },
+};
 
+const compare = ({ key, order }) => {
+  return (a, b) => {
+    let result = 0;
+
+    if (a[key] >= b[key]) result = 1;
+    else result = -1;
+
+    if (order === 'asc') return result;
+    else return result * -1;
+  };
+};
 export default {
-    state,
-    mutations,
-    getters,
-    actions
-}
+  state,
+  mutations,
+  getters,
+  actions,
+};
