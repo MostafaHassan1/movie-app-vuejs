4981eea4d05e4924df9e3624ff00f35ca01b4477
mostafa hassan <mostafahassan999888@gmail.com>
2021-02-24 02:22:56 +0200

    Added firebase

---
+++ src/services/firebase.js
@@ -0,0 +1,14 @@
+  import firebase from 'firebase'
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDo9TAfRmeFFNEC9FgoNFKsqFcb97yLzFE",
+    authDomain: "movie-app-vue-a08b9.firebaseapp.com",
+    projectId: "movie-app-vue-a08b9",
+    storageBucket: "movie-app-vue-a08b9.appspot.com",
+    messagingSenderId: "429104045898",
+    appId: "1:429104045898:web:54faf7c3db39b2522febb3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase
